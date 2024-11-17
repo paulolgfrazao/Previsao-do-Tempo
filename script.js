8f8a442c780fbdcbce7776c9df5f48383cb063c3
paulolgfrazao <paulolgf2@gmail.com>
2024-11-17 18:37:14 -0300

    Adicionando projeto
    Adicionando projeto de previsão do tempo

---
+++ script.js
@@ -0,0 +1,33 @@
+
+let chave = "c99eabc9c1897d8dbec5ea166f360819"
+
+
+function colocarNaTela(dados){
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".text-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".pais").innerHTML = dados.sys.country
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+}
+
+async function buscarCidade(cidade){
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + 
+    cidade + 
+    "&appid=" + 
+    chave + 
+    "&lang=pt_br" +
+    "&units=metric"
+    )
+    .then(resposta => resposta.json())
+
+    colocarNaTela(dados)
+}
+
+
+function cliqueiNoBotao(){
+   let cidade = document.querySelector(".input-cidade").value
+
+   buscarCidade(cidade)
+}
